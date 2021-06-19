88ac145701495affebedab3e6c5cfd41d291d2ba
Raidro Manchester <raibo.quadrofus@gmail.com>
2021-06-19 11:38:22 +0300

    gtk4: port simple_example
    Issue: connecting "destroy" to app.quit doesn't work because too many
    arguments, but works in the exact same exmaple in button_example.py

--- examples/simple_example.py
+++ examples/simple_example.py
@@ -3,7 +3,17 @@ import gi
 gi.require_version("Gtk", "4.0")
 from gi.repository import Gtk
 
-win = Gtk.Window()
-win.connect("destroy", Gtk.main_quit)
-win.show_all()
-Gtk.main()
+
+def on_activate(app):
+    win = Gtk.Window()
+    # TODO: for some reason here it gives 2 arguments, which is too much
+    # but works in "button_example.py"
+    win.connect("destroy", app.quit)
+    app.add_window(win)
+    win.show()
+
+
+app = Gtk.Application(application_id="org.example.myapp")
+app.connect("activate", on_activate)
+
+app.run(None)
