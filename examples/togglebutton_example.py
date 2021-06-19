f0d7dc6614393a226d6cfdb77a6ab1cfdee7c955
Raidro Manchester <raibo.quadrofus@gmail.com>
2021-06-19 12:37:55 +0300

    gtk4: port togglebutton_example

--- examples/togglebutton_example.py
+++ examples/togglebutton_example.py
@@ -7,19 +7,21 @@ from gi.repository import Gtk
 class ToggleButtonWindow(Gtk.Window):
     def __init__(self):
         Gtk.Window.__init__(self, title="ToggleButton Demo")
-        self.set_border_width(10)
 
-        hbox = Gtk.Box(spacing=6)
-        self.add(hbox)
+        hbox = Gtk.Box(spacing=6, margin_start=10, margin_end=10,
+                       margin_top=10, margin_bottom=10)
+        self.set_child(hbox)
 
-        button = Gtk.ToggleButton(label="Button 1")
+        button = Gtk.ToggleButton(label="Button 1", hexpand=True)
         button.connect("toggled", self.on_button_toggled, "1")
-        hbox.pack_start(button, True, True, 0)
+        hbox.append(button)
 
-        button = Gtk.ToggleButton(label="B_utton 2", use_underline=True)
+        button = Gtk.ToggleButton(
+            label="B_utton 2", use_underline=True, hexpand=True
+        )
         button.set_active(True)
         button.connect("toggled", self.on_button_toggled, "2")
-        hbox.pack_start(button, True, True, 0)
+        hbox.append(button)
 
     def on_button_toggled(self, button, name):
         if button.get_active():
@@ -29,7 +31,14 @@ class ToggleButtonWindow(Gtk.Window):
         print("Button", name, "was turned", state)
 
 
-win = ToggleButtonWindow()
-win.connect("destroy", Gtk.main_quit)
-win.show_all()
-Gtk.main()
+def on_activate(app):
+    win = ToggleButtonWindow()
+    win.connect("destroy", lambda b: app.quit())
+    app.add_window(win)
+    win.show()
+
+
+app = Gtk.Application(application_id="org.example.myapp")
+app.connect("activate", on_activate)
+
+app.run(None)
