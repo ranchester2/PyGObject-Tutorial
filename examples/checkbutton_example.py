d2efd19451ccaa4cce6d40bdb2cf4005532a73e4
Raidro Manchester <raibo.quadrofus@gmail.com>
2021-06-19 16:30:37 +0300

    gtk4: port checkbutton_example

--- examples/checkbutton_example.py
+++ examples/checkbutton_example.py
@@ -7,19 +7,19 @@ from gi.repository import Gtk
 class CheckButtonWindow(Gtk.Window):
     def __init__(self):
         Gtk.Window.__init__(self, title="CheckButton Demo")
-        self.set_border_width(10)
 
-        hbox = Gtk.Box(spacing=6)
-        self.add(hbox)
+        hbox = Gtk.Box(spacing=6, margin_start=10, margin_end=10,
+                       margin_top=10, margin_bottom=10)
+        self.set_child(hbox)
 
         button = Gtk.CheckButton.new_with_label("Button 1")
         button.connect("toggled", self.on_button_toggled, "1")
-        hbox.pack_start(button, False, False, 0)
+        hbox.append(button)
 
         button = Gtk.CheckButton.new_with_mnemonic("B_utton 2")
         button.set_active(True)
         button.connect("toggled", self.on_button_toggled, "2")
-        hbox.pack_start(button, False, False, 0)
+        hbox.append(button)
 
     def on_button_toggled(self, button, name):
         if button.get_active():
@@ -29,7 +29,14 @@ class CheckButtonWindow(Gtk.Window):
         print("Button", name, "was turned", state)
 
 
-win = CheckButtonWindow()
-win.connect("destroy", Gtk.main_quit)
-win.show_all()
-Gtk.main()
+def on_activate(app):
+    win = CheckButtonWindow()
+    win.connect("destroy", lambda b: app.quit())
+    app.add_window(win)
+    win.show()
+
+
+app = Gtk.Application(application_id="org.example.myapp")
+app.connect("activate", on_activate)
+
+app.run(None)
