1b58c8c2be05abd1e4e61dfb025590a6cf755afe
Raidro Manchester <raibo.quadrofus@gmail.com>
2021-06-19 16:37:57 +0300

    checkkbutton: make example show groups instead of radiobutton
    Radiobutton removed, checkbutton and other now do this

--- examples/checkbutton_example.py
+++ examples/checkbutton_example.py
@@ -8,18 +8,36 @@ class CheckButtonWindow(Gtk.Window):
     def __init__(self):
         Gtk.Window.__init__(self, title="CheckButton Demo")
 
-        hbox = Gtk.Box(spacing=6, margin_start=10, margin_end=10,
+        vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6, margin_start=10, margin_end=10,
                        margin_top=10, margin_bottom=10)
-        self.set_child(hbox)
+        self.set_child(vbox)
+
+        ungrouped_hbox = Gtk.Box(spacing=6)
 
         button = Gtk.CheckButton.new_with_label("Button 1")
         button.connect("toggled", self.on_button_toggled, "1")
-        hbox.append(button)
+        ungrouped_hbox.append(button)
 
         button = Gtk.CheckButton.new_with_mnemonic("B_utton 2")
         button.set_active(True)
         button.connect("toggled", self.on_button_toggled, "2")
-        hbox.append(button)
+        ungrouped_hbox.append(button)
+
+        vbox.append(ungrouped_hbox)
+
+        grouped_hbox = Gtk.Box(spacing=6)
+
+        grouped_button1 = Gtk.CheckButton.new_with_label("Grouped Button 1")
+        grouped_button1.connect("toggled", self.on_button_toggled, "G1")
+        grouped_hbox.append(grouped_button1)
+
+        grouped_button2 = Gtk.CheckButton.new_with_label("Grouped Button 2")
+        grouped_button2.set_active(True)
+        grouped_button2.set_group(grouped_button1)
+        grouped_button2.connect("toggled", self.on_button_toggled, "G2")
+        grouped_hbox.append(grouped_button2)
+
+        vbox.append(grouped_hbox)
 
     def on_button_toggled(self, button, name):
         if button.get_active():
