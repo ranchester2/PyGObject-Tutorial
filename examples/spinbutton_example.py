7125763c9070a515ebd200d365c3b1c8268bb61b
Raidro Manchester <raibo.quadrofus@gmail.com>
2021-06-19 15:35:25 +0300

    gtk4: port spinbutton_example

--- examples/spinbutton_example.py
+++ examples/spinbutton_example.py
@@ -7,24 +7,24 @@ from gi.repository import Gtk
 class SpinButtonWindow(Gtk.Window):
     def __init__(self):
         Gtk.Window.__init__(self, title="SpinButton Demo")
-        self.set_border_width(10)
 
-        hbox = Gtk.Box(spacing=6)
-        self.add(hbox)
+        hbox = Gtk.Box(spacing=6, margin_start=10, margin_end=10,
+                       margin_top=10, margin_bottom=10)
+        self.set_child(hbox)
 
         adjustment = Gtk.Adjustment(upper=100, step_increment=1, page_increment=10)
         self.spinbutton = Gtk.SpinButton()
         self.spinbutton.set_adjustment(adjustment)
         self.spinbutton.connect("value-changed", self.on_value_changed)
-        hbox.pack_start(self.spinbutton, False, False, 0)
+        hbox.append(self.spinbutton)
 
         check_numeric = Gtk.CheckButton(label="Numeric")
         check_numeric.connect("toggled", self.on_numeric_toggled)
-        hbox.pack_start(check_numeric, False, False, 0)
+        hbox.append(check_numeric)
 
         check_ifvalid = Gtk.CheckButton(label="If Valid")
         check_ifvalid.connect("toggled", self.on_ifvalid_toggled)
-        hbox.pack_start(check_ifvalid, False, False, 0)
+        hbox.append(check_ifvalid)
 
     def on_value_changed(self, scroll):
         print(self.spinbutton.get_value_as_int())
@@ -40,7 +40,14 @@ class SpinButtonWindow(Gtk.Window):
         self.spinbutton.set_update_policy(policy)
 
 
-win = SpinButtonWindow()
-win.connect("destroy", Gtk.main_quit)
-win.show_all()
-Gtk.main()
+def on_activate(app):
+    win = SpinButtonWindow()
+    win.connect("destroy", lambda b: app.quit())
+    app.add_window(win)
+    win.show()
+
+
+app = Gtk.Application(application_id="org.example.myapp")
+app.connect("activate", on_activate)
+
+app.run(None)
